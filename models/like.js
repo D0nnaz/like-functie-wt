9a19253e60f992aea9f7022f51d4958c5c9a2749
D0nnaz <stambassadeur@gmail.com>
2023-09-01 21:03:38 +0200

    d

--- models/like.js
+++ models/like.js
@@ -1,7 +1,7 @@
-const mongoose = require("mongoose");
+const likeSchema = new mongoose.Schema({
+  count: Number,
+});
 
-const Like = mongoose.model("Like", { count: Number });
+likeSchema.index({ count: 1 });
 
-module.exports = Like;
-
-
+const Like = mongoose.model("Like", likeSchema);
