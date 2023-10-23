c41fc49ed9a907c7c8777f3f9541d811582a83d7
D0nnaz <stambassadeur@gmail.com>
2023-10-23 14:58:20 +0200

    test

--- public/js/script.js
+++ public/js/script.js
@@ -1,13 +1,13 @@
 console.log("JS is connected");
 const socket = io();
 let liked = localStorage.getItem("liked") === "true";
-let page = window.location.pathname.substring(1); 
+let page = window.location.pathname.substring(1);
 
 function updateButtonState() {
   const button = document.getElementById("like-button");
   button.className = liked ? "liked" : "";
   button.textContent = liked ? "Unlike" : "Like";
-   button.style.color = liked ? "#7b00e6" : "#7b00e6";
+  button.style.color = liked ? "#7b00e6" : "#7b00e6";
 }
 
 document.getElementById("like-button").addEventListener("click", () => {
@@ -16,9 +16,9 @@ document.getElementById("like-button").addEventListener("click", () => {
   localStorage.setItem("liked", liked.toString());
 
   if (liked) {
-    socket.emit("incrementLike", page); 
+    socket.emit("incrementLike", page);
   } else {
-    socket.emit("decrementLike", page); 
+    socket.emit("decrementLike", page);
   }
 });
 
@@ -34,9 +34,9 @@ function setLikeContainerScale() {
   var screenWidth = window.innerWidth;
   var scaleFactor = 0.75;
 
-  if (screenWidth < 75) {
+  if (screenWidth < 110) {
     scaleFactor = 0.2;
-  } else if (screenWidth < 200) {
+  } else if (screenWidth < 250) {
     scaleFactor = 0.3;
   } else if (screenWidth < 300) {
     scaleFactor = 0.4;
@@ -45,7 +45,7 @@ function setLikeContainerScale() {
   } else if (screenWidth < 600) {
     scaleFactor = 0.7;
   }
-  
+
   var likeContainer = document.querySelector(".like-container");
   likeContainer.style.transform = "scale(" + scaleFactor + ")";
 }
